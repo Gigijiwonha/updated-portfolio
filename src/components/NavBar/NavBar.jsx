78d702b30e41a9089e44ec88752d203433e3119d
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-09-25 16:06:18 +1000

    create initial navbar structure

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './NavBar.style.css'
+import mainLogo from "../../Assets/mainLogo.png"
 
 const navBar = () => {
   return (
-    <div>
-      
+    <div className='navBar'>
+      <img className='navBar__mainLogo' src={mainLogo} alt="main logo" />
+        <ul className='navBar__menu'>
+            <li className='navBar__menu_item'>About Me</li>
+            <li className='navBar__menu_item'>Skills</li>
+            <li className='navBar__menu_item'>Projects</li>
+            <li className='navBar__menu_item'>Contact</li>
+        </ul>
     </div>
   )
 }
