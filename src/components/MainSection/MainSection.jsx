7f3e980785639846e7c429cbd5980fa780411e98
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-09-25 16:14:14 +1000

    create initial main section structure

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './MainSection.style.css'
+import profileImage from "../../Assets/profileImage.png"
 
 const mainSection = () => {
   return (
-    <div>
-      
+    <div className='mainSection'>
+        <div className='mainSection__contents'>
+            <div className='mainSection__contents_left'>
+                <h1>Good Morning</h1>
+                <p>The sun is shining bright, let's make the most of it!</p>
+            </div>
+            <div className='mainSection__contents_right'>
+                <img src={profileImage} alt="profile" />
+            </div>
+        </div>
     </div>
   )
 }
