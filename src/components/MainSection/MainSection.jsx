f9fce7bc1268dbe6f1a169de2d1a0f36b84c9fdb
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-10-01 18:41:05 +1000

    update mainsection CSS

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,52 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import './MainSection.style.css'
-import profileImage from "../../Assets/profileImage.png"
+import React, { useEffect, useState } from "react";
+import "./MainSection.style.css";
+import profileImage from "../../Assets/profileImage.png";
 
 const MainSection = () => {
+  const [greetingMessage, setGreetingMessage] = useState("");
 
-    const [greetingMessage, setGreetingMessage] = useState('');
-    
-    const greetingMessageByHour = {
-        morning : "Good Morning",
-        afternoon : "Good Afternoon",
-        evening : "Good Evening"
+  const greetingMessageByHour = {
+    morning: "Good Morning",
+    afternoon: "Good Afternoon",
+    evening: "Good Evening",
+  };
+
+  const updateGreetingMessage = () => {
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+
+    console.log("currentTime>>", currentTime);
+    console.log("currentHour>>", currentHour);
+
+    if (currentHour >= 0 && currentHour < 5) {
+      setGreetingMessage("Hello");
+    } else if (currentHour < 12) {
+      setGreetingMessage(greetingMessageByHour.morning);
+    } else if (currentHour < 18) {
+      setGreetingMessage(greetingMessageByHour.afternoon);
+    } else {
+      setGreetingMessage(greetingMessageByHour.evening);
     }
+  };
 
-    const updateGreetingMessage = () => {
-        let currentTime = new Date();
-        let currentHour = currentTime.getHours();
-
-        console.log("currentTime>>", currentTime);
-        console.log("currentHour>>", currentHour);
-
-        if (currentHour >= 0 && currentHour < 5){
-            setGreetingMessage('Hello')
-        } else if(currentHour < 12){
-            setGreetingMessage(greetingMessageByHour.morning)
-        } else if (currentHour < 18) {
-            setGreetingMessage(greetingMessageByHour.afternoon)
-        } else {
-            setGreetingMessage(greetingMessageByHour.evening)
-        }
-    }
-
-    useEffect(()=>{
-        updateGreetingMessage()
-    },[greetingMessage])
+  useEffect(() => {
+    updateGreetingMessage();
+  }, [greetingMessage]);
 
   return (
     <div className='mainSection'>
-        <div className='mainSection__container'>
-            <div className='mainSection__container_left'>
-                <h1>{greetingMessage}!</h1>
-                <p>The sun is shining bright, let's make the most of it!</p>
-            </div>
-            <div className='mainSection__container_right'>
-                <img src={profileImage} alt="profile" />
-            </div>
+      <div className='mainSection__container'>
+        <div className='mainSection__container_left'>
+          <h2>{greetingMessage}!</h2>
+          <h1>I'm Design-Tech Lover, GiGi</h1>
+          <p>
+            I'm a recent graduate skilled in design, interaction design, and
+            programming. I'm passionate about combining creativity with
+            technical know-how, and I'm excited to start my journey as a
+            Front-End Developer or Web Developer. Let's create something amazing
+            together! 🚀
+          </p>
         </div>
+        <div className='mainSection__container_right'>
+          <img
+            src={profileImage}
+            alt='profile'
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainSection
+export default MainSection;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,12 +6,13 @@ const NavBar = () => {
   return (
     <div className='navBar'>
       <img className='navBar__mainLogo' src={mainLogo} alt="main logo" />
-        <ul className='navBar__menu'>
-            <li className='navBar__menu_item'>About Me</li>
-            <li className='navBar__menu_item'>Skills</li>
-            <li className='navBar__menu_item'>Projects</li>
-            <li className='navBar__menu_item'>Contact</li>
-        </ul>
+      <span className='navBar__mainLogo_text'>Contact me!</span>
+      <ul className='navBar__menu'>
+          <li className='navBar__menu_item'>About Me</li>
+          <li className='navBar__menu_item'>Skills</li>
+          <li className='navBar__menu_item'>Projects</li>
+          <li className='navBar__menu_item'>Contact</li>
+      </ul>
     </div>
   )
 }
