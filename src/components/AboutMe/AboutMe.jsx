d776d0539f65b6405d20cda6f81758d926fabc99
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-10-02 21:46:30 +1000

    update mainsection contents

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import './AboutMe.style.css'
+import React from "react";
+import "./AboutMe.style.css";
 
 const AboutMe = () => {
   return (
-    <div>
-      
+    <div className='aboutMe'>
+      <div className='aboutMe__container'>
+        <h1>About Me</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, esse.</p>
+        <ul>
+          <li>
+            <i></i>
+            <h2></h2>
+            <p></p>
+          </li>
+          <li>
+            <i></i>
+            <h2></h2>
+            <p></p>
+          </li>
+          <li>
+            <i></i>
+            <h2></h2>
+            <p></p>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -37,21 +37,33 @@ const MainSection = () => {
     <div className='mainSection'>
       <div className='mainSection__container'>
         <div className='mainSection__container_left'>
-          <h2>{greetingMessage}!</h2>
-          <h1>I'm Design-Tech Lover, GiGi</h1>
+          <h3>{greetingMessage}!</h3>
+          <h1>
+            const todayGrowingPains = () =&gt; &#123; return &apos;growing 1%
+            stronger&apos;; &#125;
+          </h1>
           <p>
-            I'm a recent graduate skilled in design, interaction design, and
-            programming. I'm passionate about combining creativity with
-            technical know-how, and I'm excited to start my journey as a
-            Front-End Developer or Web Developer. Let's create something amazing
-            together! 🚀
+            This code represents efficient performance like O(1) in Big O
+            notation. Daily growing pains lead to a 1% growth, allowing for
+            steady progress. When looking at the long term, I aim for consistent
+            and stable growth. Just like O(1), which maintains its performance
+            regardless of input size, I strive to develop consistently in a
+            changing environment. Even with new technologies emerging every day,
+            I avoid being overconfident and focus on learning continuously to
+            grow efficiently. I always aim to produce reliable results, becoming
+            a solid developer through sincere and deep approaches.
           </p>
         </div>
         <div className='mainSection__container_right'>
           <img
+            className='mainSection__container_profileImg'
             src={profileImage}
             alt='profile'
           />
+          <div className='mainSection__container_profile'>
+            <h1>GiGi Jiwon Ha</h1>
+            <p>Design-Tech Lover</p>
+          </div>
         </div>
       </div>
     </div>
