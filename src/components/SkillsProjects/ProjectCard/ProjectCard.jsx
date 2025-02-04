cf637036b21f80a0a79905591694da2af5fdc832
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2025-02-04 12:29:02 +1100

    Modify UI layout in  media query

--- src/components/SkillsProjects/ProjectCard/ProjectCard.jsx
+++ src/components/SkillsProjects/ProjectCard/ProjectCard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProjectCard.style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const ProjectCard = ({ url, img, git, title, description, tools }) => {
+  const [isActivated, setIsActivated] = useState(window.innerWidth <= 768);
+
   const buttonText =
     title === "Spotify-Demo"
       ? "Demo Video"
@@ -9,17 +13,24 @@ const ProjectCard = ({ url, img, git, title, description, tools }) => {
       ? "Coming soon"
       : "Website";
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsActivated(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <li className='projects__list-item'>
-      <a
-        href={url}
-        target='_blank'
-      >
+      <a>
         <img
           src={img}
           alt={title}
         />
-        <div className='project__metadata'>
+       
+        <div className={`project__metadata ${isActivated ? "activated" : ""}`}>
           <h3 className='project__title'>{title}</h3>
           <p className='project__description'>{description}</p>
           <ul className='project__tools'>
