c417104961e61402efe8680a90c7f2334d9fe09c
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2025-02-10 21:48:32 +1100

    style: modify overall UI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,46 +9,48 @@ const Footer = ({ scrollToSection, refs }) => {
   return (
     <div className='footerSection'>
       <div className='footer__container'>
-        <div className='footer__container--left'>
-          <img
-            src={mainLogoStroke}
-            alt='Main Logo'
-            className='footer__logo'
-          />
-          <p>There's will, there's always the way</p>
-        </div>
-        <div className='footer__container--right'>
-          <ul className='footer__containter--navList'>
-            <li onClick={() => scrollToSection(homeRef)}>Home</li>
-            <li onClick={() => scrollToSection(aboutMeRef)}>About Me</li>
-            <li onClick={() => scrollToSection(skillsRef)}>Skills</li>
-            <li onClick={() => scrollToSection(projectsRef)}>projects</li>
-            <li onClick={() => scrollToSection(contactRef)}>Contact Me</li>
-          </ul>
-          <div className='footer__containter--icons'>
-            <a
-              href={"https://github.com/Gigijiwonha"}
-              target='_blank'
-            >
-              <FontAwesomeIcon
-                icon={faSquareGithub}
-                className='footer__icon'
-              />
-            </a>
-            <a
-              href={"https://www.linkedin.com/in/gigi-jiwon-ha/"}
-              target='_blank'
-            >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className='footer__icon'
-              />
-            </a>
+        <div className='footer__content'>
+          <div className='footer__content--left'>
+            <img
+              src={mainLogoStroke}
+              alt='Main Logo'
+              className='footer__logo'
+            />
+            <p>There's will, there's always the way</p>
+          </div>
+          <div className='footer__content--right'>
+            <ul className='footer__content--navList'>
+              <li onClick={() => scrollToSection(homeRef)}>Home</li>
+              <li onClick={() => scrollToSection(aboutMeRef)}>About Me</li>
+              <li onClick={() => scrollToSection(skillsRef)}>Skills</li>
+              <li onClick={() => scrollToSection(projectsRef)}>projects</li>
+              <li onClick={() => scrollToSection(contactRef)}>Contact Me</li>
+            </ul>
+            <div className='footer__content--icons'>
+              <a
+                href={"https://github.com/Gigijiwonha"}
+                target='_blank'
+              >
+                <FontAwesomeIcon
+                  icon={faSquareGithub}
+                  className='footer__icon'
+                />
+              </a>
+              <a
+                href={"https://www.linkedin.com/in/gigi-jiwon-ha/"}
+                target='_blank'
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className='footer__icon'
+                />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='footer__copyright'>
-        <p>© 2024 Gigi Jiwon Ha Portfolio. All rights reserved.</p>
+        <div className='footer__copyright'>
+          <p>© 2024 Gigi Jiwon Ha Portfolio. All rights reserved.</p>
+        </div>
       </div>
     </div>
   );
